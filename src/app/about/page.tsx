5d136b42c4785db72eb07fabce8988158aafbc02
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-07-30 01:41:45 +0800

    feat: create about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,12 +9,12 @@ export const metadata: Metadata = {
 
 export default function Blog() {
   return (
-    <section className="min-h-full mb-24">
+    <section className="min-h-full min-w-screen min-h-screen">
       <article className="w-full p-12">
         <h1 className="text-[2.986rem] uppercase font-bold">About us</h1>
         <p className="text-lg font-normal">We are a social media company that wants to bring people together!</p>
       </article>
-      <Image sizes="(max-width: 600px) 300px" src={'/Diverse.jpg'} width={1600} height={898} alt="Diverse happy four people laughing while sitting on a couch photograph by rawpixel" priority/>
+      <Image sizes="(max-width: 600px) 300px" src={'/Diverse.jpg'} width={1600} height={898} style={{ width: '100%', height: 'auto' }} alt="Diverse happy four people laughing while sitting on a couch photograph by rawpixel" priority/>
     </section>
   )
 }
