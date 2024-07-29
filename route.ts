2c36487c9c7930bf21464e1ff84621e387dd1e50
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-07-30 01:46:31 +0800

    feat: create route for route protection

---
+++ route.ts
@@ -0,0 +1,12 @@
+export const publicRoutes = [
+  "/"
+];
+
+export const authRoutes = [
+  "/api/auth/signin",
+  "/api/auth/register"
+];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/";
